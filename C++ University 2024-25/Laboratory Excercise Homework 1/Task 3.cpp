6db13c9293b7d18c9736129c33f9f797015f1d97
rados <radostnchalakutev@gmail.com>
2024-11-16 17:54:38 +0200

    In progress v1.0

--- C++ University 2024-25/Laboratory Excercise Homework 1/Task 3.cpp
+++ C++ University 2024-25/Laboratory Excercise Homework 1/Task 3.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 
 int main() {
-    int a;
-    cin >> a;
-    int b;
-    cin >> b;
-    int c;
-    cin >> c;
+    int x;
+    cin >> x;
+    int y;
+    cin >> y;
+    int z;
+    cin >> z;
+    int lcmOfTwo=0;
+    int gcd=0;
+
+    for (int i = 1; i <=x && i<=y && i<=z; ++i) {
+        if (x%i==0 && y%i==0 && z%i==0) {
+            gcd=i;
+            lcmOfTwo=x*y/gcd;
+        }
+    }
+    if (x<10 ||x>100 || y<10 || y>100 || z<10 || z>100) {
+        cout<<"Invalid input data!"<<endl;
+    }else {
+        int lcm;
+        lcm=lcmOfTwo*z/gcd;
+        cout<<lcm<<endl;
+    }
+    return 0;
+
+
 
     
 }
